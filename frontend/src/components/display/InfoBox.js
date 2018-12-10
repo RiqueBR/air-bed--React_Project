ab568b9d031b7c43a027cffe13af8d135758586b
Raymond Yau <raymondccyau@gmail.com>
2018-12-10 16:50:53 +0000

    latest front-end changes

--- frontend/src/components/display/InfoBox.js
+++ frontend/src/components/display/InfoBox.js
@@ -4,10 +4,12 @@ import PropertyBox from './property/PropertyBox';
 import ExperienceBox from './experience/ExperienceBox'
 
 
-const InfoBox = (props) => {
+const InfoBox = ({data}) => {
+  if(!data) return null;
+  console.log(data);
   return(
     <div className="info-box">
-      <PropertyBox />
+      <PropertyBox data={data._embedded.properties}/>
       <ExperienceBox />
     </div>
   )
